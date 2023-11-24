1872d32615fc4907b8c96774d6a2336cea5d7df1
davidvaldh <ds.valderrama@uniandes.edu.co>
2023-11-24 09:01:18 -0500

    Feat: Logic without relations

--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,4 +1,41 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
+import { Repository } from 'typeorm';
+import { AlbumEntity } from './album.entity';
 
 @Injectable()
-export class AlbumService {}
+export class AlbumService {
+   constructor(
+       @InjectRepository(AlbumEntity)
+       private readonly albumRepository: Repository<AlbumEntity>
+   ){}
+
+   async findAll(): Promise<AlbumEntity[]> {
+       return await this.albumRepository.find({ relations: ["tracks", "performers"] });
+   }
+
+   async findOne(id: string): Promise<AlbumEntity> {
+       const album: AlbumEntity = await this.albumRepository.findOne({where: {id}, relations: ["tracks", "performers"] } );
+       if (!album)
+         throw new BusinessLogicException("The album with the given id was not found", BusinessError.NOT_FOUND);
+  
+       return album;
+   }
+  
+   async create(album: AlbumEntity): Promise<AlbumEntity> {
+       return await this.albumRepository.save(album);
+   }
+
+   async delete(id: string) {
+       const album: AlbumEntity = await this.albumRepository.findOne({where:{id}});
+       if (!album)
+         throw new BusinessLogicException("The album with the given id was not found", BusinessError.NOT_FOUND);
+
+        if (album.tracks.length > 0)
+            throw new BusinessLogicException("The album has tracks associated", BusinessError.PRECONDITION_FAILED);
+    
+       await this.albumRepository.remove(album);
+   }
+}
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,4 +1,39 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TrackEntity } from './track.entity';
+import { Repository } from 'typeorm';
+import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
+import { AlbumEntity } from '../album/album.entity';
 
 @Injectable()
-export class TrackService {}
+export class TrackService {
+
+    constructor(
+        @InjectRepository(TrackEntity)
+        private readonly trackRepository: Repository<TrackEntity>,
+        @InjectRepository(AlbumEntity)
+        private readonly albumRepository: Repository<AlbumEntity>,
+    ) { }
+
+    async findAll(): Promise<TrackEntity[]> {
+        return await this.trackRepository.find({ relations: ["album"] });
+    }
+
+    async findOne(id: string): Promise<TrackEntity> {
+        const track: TrackEntity = await this.trackRepository.findOne({ where: {id}, relations: ["album"] });
+        if (!track)
+            throw new BusinessLogicException("The track with the given id was not found", BusinessError.NOT_FOUND);
+
+        return track;
+    }
+
+    async create(track: TrackEntity, albumId: string): Promise<TrackEntity> {
+        const album = await this.albumRepository.findOne({where: {id: albumId}});
+        if (!album)
+            throw new BusinessLogicException("The album with the given id was not found", BusinessError.NOT_FOUND);
+        track.album = album;
+        return await this.trackRepository.save(track);
+    }
+
+}
